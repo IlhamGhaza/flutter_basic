74e426781076887d44c3ccf9a726e1e233358973
Ilham Ghazali <m.ilhamghazali@gmail.com>
2024-05-11 21:52:09 +0700

    feat: Add PaddingWidget, RowWidget, StackWidget, and SizedBoxWidget for basic layout examples

---
+++ lib/widget_lesson/basic_layout/row_widget.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter/material.dart';
+
+class RowWidget extends StatelessWidget {
+  const RowWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('JagoFlutter - Row'),
+        actions: const [],
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(17.0)
+                ),
+                child: IconButton(
+                  icon: const Icon(Icons.arrow_back_ios),
+                  onPressed: () {},
+                ),
+            ),
+            const Text(
+              "details",
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.normal
+              ),
+            ),
+            IconButton(
+              onPressed: (){}, 
+              icon: const Icon(Icons.share, size: 32.0,
+              )
+            )
+          ],
+      ),
+    ));
+  }
+}
